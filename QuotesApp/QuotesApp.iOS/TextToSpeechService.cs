9784bbf97de2abe724aa8f36435222a195f3427e
igor <iroztropinski@inovatica.com>
2018-06-07 08:14:42 +0200

    iOS text to speech

---
+++ QuotesApp/QuotesApp.iOS/TextToSpeechService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AVFoundation;
+using Foundation;
+using QuotesApp.Service.Abstraction;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(QuotesApp.iOS.TextToSpeechService))]
+namespace QuotesApp.iOS
+{
+    public class TextToSpeechService : ITextToSpeechService
+    {
+        public void Speak(string text)
+        {
+            var speechSynthesizer = new AVSpeechSynthesizer();
+            var speechUtterance = new AVSpeechUtterance(text)
+            {
+                Rate = AVSpeechUtterance.MaximumSpeechRate / 4,
+                Voice = AVSpeechSynthesisVoice.FromLanguage("en-US"),
+                Volume = 0.5f,
+                PitchMultiplier = 1.0f
+            };
+            speechSynthesizer.SpeakUtterance(speechUtterance);
+        }
+    }
+}
